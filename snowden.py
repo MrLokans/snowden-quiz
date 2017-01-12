319359bbc635ba6131fd07e949ce511a40b5e05e
MrLokans <trikster1911@gmail.com>
2017-01-12 21:28:17 +0300

    Added brut-force solution with n^2 time complexity.

--- snowden.py
+++ snowden.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import os
+import itertools
 import logging
 import argparse
 
@@ -10,11 +11,38 @@ logger = logging.getLogger(__file__)
 
 class SnowdenSolver(object):
 
+    @staticmethod
+    def _is_list_encrypted(char_lists):
+        for el in char_lists:
+            if len(el) > 1:
+                return True
+        return False
+
+    @staticmethod
+    def _remove_duplicates(char_lists):
+        new_list = []
+        for indx, el in enumerate(char_lists):
+            if len(el) > 1:
+                if len(el) % 2 == 0:
+                    continue
+                else:
+                    new_list.append(el[0])
+            else:
+                new_list.extend(el)
+        return new_list
+
     def solve(self, encrypted_text):
         """
         Decrypts encoded text
+
+        Worst-case scenario is when our impurt string
+        is a palindrome without repeated symbols
         """
-        return ""
+        groupped = [list(g) for _, g in itertools.groupby(encrypted_text)]
+        while self._is_list_encrypted(groupped):
+            groupped = self._remove_duplicates(groupped)
+            groupped = [list(g) for _, g in itertools.groupby(groupped)]
+        return "".join(itertools.chain(*groupped))
 
 
 def parse_args():
